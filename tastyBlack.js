b499eb26a3b63f41275ee4661d261906bf6b7237
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2020-08-12 13:49:26 +0900

    Update tastyBlack.js

--- tastyBlack.js
+++ tastyBlack.js
@@ -1,3 +1,5 @@
+
+//透明度を変化させる
 let header = document.getElementById("header");
 let isPositive = true;
 let opacity = 0; // 不透明度
@@ -13,11 +15,10 @@ function opacityAnimate() {
     isPositive = false;
   } else if (opacity < 0) {
     isPositive = true;
-  }
+ }
 
-  header.style.opacity = opacity;
+   header.style.opacity = opacity;
 }
-
 // 300ミリ秒ごとに opacityAnimate 関数を実行する
 setInterval(opacityAnimate, 300);
 
@@ -25,37 +26,37 @@ setInterval(opacityAnimate, 300);
 //回答欄とその下部分
 'use strict';
 const answerInput = document.getElementById('answer');
-const assessmentButton =    document.getElementById('assessment');
+const assessmentButton = document.getElementById('assessment');
 const resultDivided = document.getElementById('result-area');
 
 assessmentButton.onclick = ( ) => {
 const answer = answerInput.value;
 
-     if(answer.length===0) { //空欄の時は処理を終了
+   if(answer.length===0) { //空欄の時は処理を終了
         return;
-    };
+   };
     
-   if(answer==='かごしま') {   //正解の時のみ、応募フォームを表示する
-        document.getElementById('result-area').innerHTML=('大正解！！　九州 かごしまの「黒豚」「黒酢」「黒さつま鶏」「うなぎ」他、盛りだくさんの中から<br>うまか黒を お選びください！ ＊==============＊ <a href = "https://www.nnn.ed.nico" target="_blank"> ご応募はこちら(N予備校)</a> '); 
+  //正解の時のみ、応募フォームを表示
+  //正解は、 ’かごしま' ・地元の人の言い方 'かごんま' のときのみ
+  if(answer==='かごしま' || 'かごんま'===answer) { 
+           document.getElementById('result-area').innerHTML=('大正解！！ 南国 鹿児島の "黒豚" "黒牛" "黒さつま鶏" "黒酢"  "うなぎ" 他、盛りだくさんの中から<br>うんまか~黒を お選びください！ ＊===================＊ <a href = "https://www.nnn.ed.nico" target="_blank"> ご応募はこちら(N予備校)</a> '); 
     }else{
         document.getElementById('result-area').innerHTML="おしいです！　再度挑戦してみてください！";
     };
  };
  
- //エンターキーでも機能する
+  //エンターキーでも機能する
+  answerInput.onkeydown = event => {
+    if (event.keyCode === 13) {
+      assessmentButton.onclick();
+    }
+ };
+  
+ //エンターを日本語変換完了と区別するため
+ //上記コードに変更
  //answerInput.onkeydown = event => {
-    // if (event.key === 'Enter') {
-        // assessmentButton.onclick();
-    //  }
-//};
-
-//エンターキーのとき、
-//日本語変換完了のEnterと区別したいので試作中
-//answerInput.onkeydown = event => {
-       // if ( 13 === event.keyDownCode  &&  event.keyUpCode===13) {
-       // assessmentButton.onclick();
-      //  } else {
-      //  return;
-       // }
+   // if (event.key === 'Enter') {
+         //assessmentButton.onclick();
+     // }
 //};
 
